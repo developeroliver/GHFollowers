7e164d4edef3226b59a3b7649c7220ee14b929f9
olivier geiger <developpeurolive.ios@gmail.com>
2024-03-14 14:48:52 +0100

    Refactoring error messages with Enum clear text field after validation

--- GHFollowers/Screens/SearchVC.swift
+++ GHFollowers/Screens/SearchVC.swift
@@ -97,6 +97,7 @@ class SearchVC: UIViewController {
 extension SearchVC: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         pushFollowerListVC()
+        textField.text = ""
         return true
     }
 }
