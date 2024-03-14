d19842759cfc427c3f41bde37e07581a8c077529
olivier geiger <developpeurolive.ios@gmail.com>
2024-03-14 13:38:05 +0100

    Create User & Follower Model

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by olivier geiger on 14/03/2024.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
