d96a6467035f5226ee5fa307f51f995e5508fddb
olivier geiger <developpeurolive.ios@gmail.com>
2024-03-14 11:40:05 +0100

    add SearchVC and FavoritesVC and create TabBar Controller

---
+++ GHFollowers/FavoritesVC.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritesVC.swift
+//  GHFollowers
+//
+//  Created by olivier geiger on 14/03/2024.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
---
+++ GHFollowers/SearchVC.swift
@@ -0,0 +1,16 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by olivier geiger on 14/03/2024.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
