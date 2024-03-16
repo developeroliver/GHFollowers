83d22879d51034fc668cab0f697fa14701a65d0c
olivier geiger <developpeurolive.ios@gmail.com>
2024-03-16 10:40:11 +0100

    Modal presentation for UserVC and passing data

---
+++ GHFollowers/Screens/UserInfoVC.swift
@@ -0,0 +1,27 @@
+//
+//  UserInfoVC.swift
+//  GHFollowers
+//
+//  Created by olivier geiger on 16/03/2024.
+//
+
+import UIKit
+
+class UserInfoVC: UIViewController {
+    
+    var username: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
+        navigationItem.rightBarButtonItem = doneButton
+        
+        print(username)
+    }
+    
+
+    @objc func dismissVC() {
+        dismiss(animated: true)
+    }
+}
