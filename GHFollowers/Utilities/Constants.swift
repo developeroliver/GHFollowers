adb208ef2757f919f93492361341572a42766519
olivier geiger <developpeurolive.ios@gmail.com>
2024-03-16 12:21:44 +0100

    Create ChildViewController et UserInfoHeaderVC

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by olivier geiger on 16/03/2024.
+//
+
+import UIKit
+
+enum SFSymbols {
+    static let location         = "mappin.and.ellipse"
+}
+
+
+enum TextString {
+    static let noLocation       = "No Location"
+    static let noBioAvailable   = "No bio available"
+}
