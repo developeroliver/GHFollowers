9a49f1a3b7421a9ef59782e4c153dd2467e0929a
olivier geiger <developpeurolive.ios@gmail.com>
2024-03-17 10:50:01 +0100

    Create PersistenceManager to add or remove a favorite

--- GHFollowers/Utilities/GFError.swift
+++ GHFollowers/Utilities/GFError.swift
@@ -8,8 +8,10 @@
 import UIKit
 
 enum GFError: String, Error {
-    case invalidUsername  = "Ce nom d'utilisateur a créé une demande non valide. Veuillez réessayer."
-    case unableToComplete = "Impossible de compléter votre demande. Veuillez vérifier votre connexion internet."
-    case invalidResponse  = "Réponse invalide du serveur. Veuillez réessayer."
-    case invalidData      = "Les données reçues du serveur n'étaient pas valides. Veuillez réessayer."
+    case invalidUsername        = "Ce nom d'utilisateur a créé une demande non valide. Veuillez réessayer."
+    case unableToComplete       = "Impossible de compléter votre demande. Veuillez vérifier votre connexion internet."
+    case invalidResponse        = "Réponse invalide du serveur. Veuillez réessayer."
+    case invalidData            = "Les données reçues du serveur n'étaient pas valides. Veuillez réessayer."
+    case unableToFavorite       = "Une erreur s'est produite lors de la mise en favori de cet utilisateur. Veuillez réessayer."
+    case alreadyInFavorites     = "Vous avez déjà favorisé cet utilisateur. Vous devez VRAIMENT l'apprécier!"
 }
