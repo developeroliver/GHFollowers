cc39e32587fa03349719b61813948a20a591fe87
codewitholivier as Oliver <92441827+developeroliver@users.noreply.github.com>
2024-12-11 19:46:08 +0100

    GFButton and GFTextfield

---
+++ GHFollowers/GHFollowers/GFTextField.swift
@@ -0,0 +1,20 @@
+//
+//  GFTextField.swift
+//  GHFollowers
+//
+//  Created by olivier geiger on 11/12/2024.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
